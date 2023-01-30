51bdb3cbb81456a3414c5e274b2a6d87814e28ae
BahnMiFPS <87252277+BahnMiFPS@users.noreply.github.com>
2023-01-30 16:58:46 +1100

    demo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react"
+import "./App.css"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [count, setCount] = useState(0)
+	const decrement = () => setCount(count - 1)
+	const increment = () => setCount(count + 1)
+	const resetCount = () => setCount(0)
+	let color = count < 0 ? "red" : count > 0 ? "green" : "white"
+
+	return (
+		<div className="container">
+			<div className="title">
+				<h1>Counter</h1>
+			</div>
+			<div className="number">
+				<h1 id="value" style={{ color }}>
+					{count}
+				</h1>
+			</div>
+			<div className="btn-group">
+				<button className="decrease btn button-6" onClick={decrement}>
+					Decrease
+				</button>
+				<button className="reset btn button-6" onClick={resetCount}>
+					Reset
+				</button>
+				<button className="increase btn button-6" onClick={increment}>
+					Increase
+				</button>
+			</div>
+		</div>
+	)
 }
 
-export default App;
+export default App
